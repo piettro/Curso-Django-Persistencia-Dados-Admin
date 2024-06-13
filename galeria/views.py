82e0f91d7f5dc7fddf8117de20a11b566eb933ea
piettro <piettroenrico@hotmail.com>
2024-06-13 08:12:59 -0300

    Class1

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'galeria/index.html')
+def index(request):    
+    data = {
+        1: {
+            "nome":"Nebulosa de Carina",
+            "legenda":"webbtelescope.org / NASA / James Web"
+        },
+        2: {
+            "nome":"Galáxia NGC 1079",
+            "legenda":"nasa.org / NASA / Hubble"
+        }
+    }
+    
+    return render(request, 'galeria/index.html', {"cards":data})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
